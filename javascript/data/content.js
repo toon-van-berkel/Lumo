03a3c06439db10fa62a37f603731cb7854e8b781
Toon van Berkel <toonvanberkel203@gmail.com>
2026-04-14 10:37:41 +0200

    Taal switch (#6)
    * Add under construction page
    
    * Updated README.md
    
    I updated README.md to the context of the project
    
    * Fixed grammer mistake in link
    
    * Create CNAME
    
    * Created the homepage for LUMO
    
    * Added the notification function
    
    Notifications are a function from Connect.LUMO
    
    * Pushing assets like logo and banner
    
    * Styling homepage
    
    Created styling for the homepage and a bit for the support
    
    * Taal switch toegevoegd
    
    ---------
    
    Signed-off-by: Toon van Berkel <toonvanberkel203@gmail.com>

---
+++ javascript/data/content.js
@@ -0,0 +1,58 @@
+export let content = {
+    'en-gb': {
+        'nav': {
+            'link1':            'Home',
+            'link2':            'Guide',
+            'imgAlt':           'Logo of Lumo',
+            'link3':            'Support'
+        },
+        'footer': {
+            'imgAlt':           'Logo of Lumo',
+            'link1':            'Controls',
+            'link2':            'Guide',
+            'link3':            'Contact',
+            'link4':            'Support',
+            'link5':            'Privacy',
+            'link6':            'General terms and conditions',
+            'link7':            'Cookies'
+        },
+        'connect': {
+            'connectButton':    'Connect Lumo',
+        },
+        'connectingComponent': {
+            'gifAlt':           'Loading animation',
+            'title':            'Connecting Lumo',
+            'message':          "We are connecting your Lumo, this might take a while. If it won't connect in 5 minutes ",
+            'link':             'make a ticket',
+            'badge':            'We are trying to connect you to our services.'
+        }
+    },
+    'nl-nl': {
+        'nav': {
+            'link1':            'Thuis',
+            'link2':            'Bediening',
+            'imgAlt':           'Logo van Lumo',
+            'link3':            'Hulp'
+        },
+        'footer': {
+            'imgAlt':           'Logo of Lumo',
+            'link1':            'Bediening',
+            'link2':            'Handleiding',
+            'link3':            'Contact',
+            'link4':            'Hulp',
+            'link5':            'Privacy',
+            'link6':            'Algemene voorwaarden',
+            'link7':            'Cookies'
+        },
+        'connect': {
+            'connectButton':    'Verbind Lumo',
+        },
+        'connectingComponent': {
+            'gifAlt':           'Verbinden animatie',
+            'title':            'Verbinden met Lumo',
+            'message':          "Wij zijn aan het verbinden met uw Lumo, dit kan even duren. Als het niet verbind binnen 5 minuten ",
+            'link':             'maak dan een ticket',
+            'badge':            'Wij proberen uw te verbinden met ons systeem.'
+        }
+    }
+};
